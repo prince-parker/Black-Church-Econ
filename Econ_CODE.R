e0b070c4ddfe5266ed280038a93acb659fb867b0
prince-parker <prince.t.parker02@gmail.com>
2026-02-23 10:55:13 -0500

    code update

--- Econ_CODE.R
+++ Econ_CODE.R
@@ -3,4 +3,4 @@ econ2018 <- da03471.0001 %>% filter(YEAR == "2018")
 
 write.csv(econ2018, "/Users/princeparker/Downloads/Coursework/Econ Dev/BC_Econ/econ2018.csv", row.names = FALSE)
 
-econ2018
+econ2018rev <- econ2018 %>% select(OWNMONY, )
